c2daef7899684b6a11354ab14959d727760b8b4f
Zahoor Ahmad <i.am.zahoor.a@outlook.com>
2023-08-20 14:39:48 +0500

    task1 started

---
+++ assignment 3/task1.js
@@ -0,0 +1,6 @@
+"use strict";
+function spliceFunc(arr, index, value) {
+    return arr.splice(index, 0, value);
+}
+console.log(spliceFunc([1, 2, 3], 1, 5));
+function spliceFuncAgainTried(arr) { }
---
+++ assignment 3/task1.ts
@@ -0,0 +1,6 @@
+function spliceFunc(arr:number[],index:number,value:number){
+    return arr.splice(index,0,value)
+}
+console.log(spliceFunc([1,2,3],1,5))
+
+function spliceFuncAgainTried(arr:number[]){}
