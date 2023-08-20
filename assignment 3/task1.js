de1f01ac5bbe80a2d01702546f69941ec4247d06
Zahoor Ahmad <i.am.zahoor.a@outlook.com>
2023-08-20 15:30:41 +0500

    task1 completed

--- assignment 3/task1.js
+++ assignment 3/task1.js
@@ -2,5 +2,9 @@
 function spliceFunc(arr, index, value) {
     return arr.splice(index, 0, value);
 }
-console.log(spliceFunc([1, 2, 3], 1, 5));
-function spliceFuncAgainTried(arr) { }
+let myArray = [1, 2, 3, 4];
+// console.log(spliceFunc(myArray,1,5))
+console.log("Array: ", myArray);
+const result = spliceFunc(myArray, 2, 4);
+console.log("New Array: ", myArray);
+// function spliceFuncAgainTried(arr:number[]){}
--- assignment 3/task1.ts
+++ assignment 3/task1.ts
@@ -1,6 +1,10 @@
-function spliceFunc(arr:number[],index:number,value:number){
+function spliceFunc(arr:number[],index:number,value:number):number[]{
     return arr.splice(index,0,value)
 }
-console.log(spliceFunc([1,2,3],1,5))
+let myArray:number[]=[1,2,3,4]
+// console.log(spliceFunc(myArray,1,5))
+console.log("Array: ",myArray)
+const result:number[] = spliceFunc(myArray,2,4)
+console.log("New Array: ",myArray)
 
-function spliceFuncAgainTried(arr:number[]){}
+// function spliceFuncAgainTried(arr:number[]){}
