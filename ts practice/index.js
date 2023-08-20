6bed0a1c4ff927e5368b56711654c23eb9467ebf
Zahoor Ahmad <i.am.zahoor.a@outlook.com>
2023-08-20 16:13:41 +0500

    prefix postfix logic

--- ts practice/index.js
+++ ts practice/index.js
@@ -1,10 +1,15 @@
 "use strict";
-// functions
-function friends(person1, person2) {
-    let friend = `${person1} and ${person2} are best friends`;
-    return friend;
-}
-console.log(friends("Ahmad Saboor", "Zahoor Ahmad"));
-// Arrays
-let classStudents = ["Ahmad Saboor", "Zahoor Ahmad", "Muhammad Talha", "Muhammad Faizan", "Waqar Akram Bhutta",];
-console.log(classStudents);
+// // functions
+// function friends(person1: string, person2: string) {
+//     let friend = `${person1} and ${person2} are best friends`
+//     return friend
+// }
+// console.log(friends("Ahmad Saboor", "Zahoor Ahmad"))
+// // Arrays
+// let classStudents: string[] = ["Ahmad Saboor", "Zahoor Ahmad", "Muhammad Talha", "Muhammad Faizan", "Waqar Akram Bhutta",]
+// console.log(classStudents)
+let a = 1;
+a++;
+let b = 1 + ++a;
+console.log("a: ", a);
+console.log("b: ", b);
--- ts practice/index.ts
+++ ts practice/index.ts
@@ -1,12 +1,24 @@
-// functions
-function friends(person1: string, person2: string) {
-    let friend = `${person1} and ${person2} are best friends`
-    return friend
-}
-console.log(friends("Ahmad Saboor", "Zahoor Ahmad"))
+// // functions
+// function friends(person1: string, person2: string) {
+//     let friend = `${person1} and ${person2} are best friends`
+//     return friend
+// }
+// console.log(friends("Ahmad Saboor", "Zahoor Ahmad"))
 
 
 
-// Arrays
-let classStudents: string[] = ["Ahmad Saboor", "Zahoor Ahmad", "Muhammad Talha", "Muhammad Faizan", "Waqar Akram Bhutta",]
-console.log(classStudents)
+// // Arrays
+// let classStudents: string[] = ["Ahmad Saboor", "Zahoor Ahmad", "Muhammad Talha", "Muhammad Faizan", "Waqar Akram Bhutta",]
+// console.log(classStudents)
+
+
+
+let a:number=1
+
+a++
+let b:number=1+ ++a
+
+console.log("a: ",a);
+console.log("b: ",b);
+
+
