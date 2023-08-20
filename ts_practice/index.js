ac7158574eadfe31aa5812b7f71c2e6f9aa506ba
Zahoor Ahmad <i.am.zahoor.a@outlook.com>
2023-08-20 16:38:16 +0500

    sum of even 100

--- ts_practice/index.js
+++ ts_practice/index.js
@@ -8,8 +8,21 @@
 // // Arrays
 // let classStudents: string[] = ["Ahmad Saboor", "Zahoor Ahmad", "Muhammad Talha", "Muhammad Faizan", "Waqar Akram Bhutta",]
 // console.log(classStudents)
+// 20 Aug
 let a = 1;
 a++;
 let b = 1 + ++a;
-console.log("a: ", a);
-console.log("b: ", b);
+// console.log("a: ",a);
+// console.log("b: ",b);
+// while (a<10) {
+//     // console.log(a)
+// }
+// create sum of even between 1 to 100
+console.log("me");
+var sum = 0;
+for (let i = 1; i <= 100; i++) {
+    if (i % 2 == 0) {
+        sum = sum + i;
+    }
+}
+console.log(sum);
