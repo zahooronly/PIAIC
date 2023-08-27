a46b17240adb7b955bf2db30cfc22ee344045c58
Zahoor Ahmad <i.am.zahoor.a@outlook.com>
2023-08-27 17:01:05 +0500

    MAP Method

--- ts_practice/27Aug/arrayMethods.js
+++ ts_practice/27Aug/arrayMethods.js
@@ -2,3 +2,13 @@
 // Map=> returns same length array
 // FILTER => returns same length or less (because it is filtered with condition)
 // FOREACH => returns nothing
+// MAP METHOD:
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+let numArray = numbers.map((num) => {
+    if (num % 2 === 0) {
+        return `${num} is Even`;
+    }
+    return `${num} is Odd`;
+});
+console.log("Original Array", numbers);
+console.log("Updated Array with Map", numArray);
