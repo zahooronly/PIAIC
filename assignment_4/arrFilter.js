bd04f4d73f764be2103a95fa9ef6b26fd4084c9b
Zahoor Ahmad <i.am.zahoor.a@outlook.com>
2023-08-27 16:51:11 +0500

    assignment_4 arrFilter

--- assignment_4/arrFilter.js
+++ assignment_4/arrFilter.js
@@ -1,23 +1,23 @@
 "use strict";
 // console.log("Hello")
-let negArr = [-1, 2, 3, -4, 5, -6, 7, -8, 9, -10];
+let negArr = [-1, 2, 3, 3, 4, 4, 4, 4, 3, 3, 3, 3, 4, 4, 4, -4, 5, -6, 7, -8, 9, -10];
 console.log("Original Array: ", negArr);
 // let negArrFilter = negArr.filter((num)=>num>=0);
-let index = 0;
-while (index < negArr.length) {
-    if (negArr[index] < 0) {
-        negArr.splice(index, 1);
+// let index:number = 0;
+// while(index<negArr.length){
+//     if(negArr[index]<0){
+//         negArr.splice(index,1);
+//     }else{
+//         index++;
+//     }
+// }
+let j = 0;
+while (j < negArr.length) {
+    if (negArr[j] % 2 == 0) {
+        negArr.splice(j, 1);
     }
     else {
-        index++;
-    }
-}
-while (index < negArr.length) {
-    if (negArr[index] % 2 == 0) {
-        negArr.splice(index, 1);
-    }
-    else {
-        index++;
+        j++;
     }
 }
 // console.log("Filtered Array: ",negArrFilter);
--- assignment_4/arrFilter.ts
+++ assignment_4/arrFilter.ts
@@ -1,21 +1,22 @@
 // console.log("Hello")
-let negArr:number[]= [-1,2,3,-4,5,-6,7,-8,9,-10];
+let negArr:number[]= [-1,2,3,3,4,4,4,4,3,3,3,3,4,4,4,-4,5,-6,7,-8,9,-10];
 console.log("Original Array: ",negArr);
 
 // let negArrFilter = negArr.filter((num)=>num>=0);
-let index:number = 0;
-while(index<negArr.length){
-    if(negArr[index]<0){
-        negArr.splice(index,1);
+// let index:number = 0;
+// while(index<negArr.length){
+//     if(negArr[index]<0){
+//         negArr.splice(index,1);
+//     }else{
+//         index++;
+//     }
+// }
+let j:number=0
+while(j<negArr.length){
+    if(negArr[j]%2==0){
+        negArr.splice(j,1);
     }else{
-        index++;
-    }
-}
-while(index<negArr.length){
-    if(negArr[index]%2==0){
-        negArr.splice(index,1);
-    }else{
-        index++;
+        j++;
     }
 }
 // console.log("Filtered Array: ",negArrFilter);
