2dbaec84c5d1b403a2ce70e3350b90f9f05ef214
Zahoor Ahmad <i.am.zahoor.a@outlook.com>
2023-08-27 16:22:35 +0500

    assignment_4 task 1

---
+++ assignment_4/arrFilter.ts
@@ -0,0 +1,24 @@
+// console.log("Hello")
+let negArr:number[]= [-1,2,3,-4,5,-6,7,-8,9,-10];
+console.log("Original Array: ",negArr);
+
+// let negArrFilter = negArr.filter((num)=>num>=0);
+let index:number = 0;
+while(index<negArr.length){
+    if(negArr[index]<0){
+        negArr.splice(index,1);
+    }else{
+        index++;
+    }
+}
+while(index<negArr.length){
+    if(negArr[index]%2==0){
+        negArr.splice(index,1);
+    }else{
+        index++;
+    }
+}
+// console.log("Filtered Array: ",negArrFilter);
+console.log("Filtered Array: ",negArr);
+
+// console.log(negArrFilter)
