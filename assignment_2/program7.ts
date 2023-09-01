c0fc2688bef8b633f7cc0a9a34fcec00ba8f15f2
Zahoor Ahmad <i.am.zahoor.a@outlook.com>
2023-09-01 15:19:06 +0500

    assignment_2 program 7

---
+++ assignment_2/program7.ts
@@ -0,0 +1,9 @@
+const temperature: number = 10;
+
+if (temperature < 10) {
+  console.log("It's cold. Wear warm clothes.");
+} else if (temperature >= 10 && temperature < 20) {
+  console.log("It's cool. A light jacket might be needed.");
+} else {
+  console.log("It's warm. Enjoy the pleasant weather.");
+}
