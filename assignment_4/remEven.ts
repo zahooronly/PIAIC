a32897ebafaad8235c11217968055efb63cec989
Zahoor Ahmad <i.am.zahoor.a@outlook.com>
2023-09-01 15:22:39 +0500

    assignment_4 remove evens

---
+++ assignment_4/remEven.ts
@@ -0,0 +1,10 @@
+const NewNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (let i = 0; i < NewNumbers.length; i++) {
+  if (NewNumbers[i] % 2 === 0) {
+    NewNumbers.splice(i, 1);
+    i--; 
+  }
+}
+
+console.log(numbers);
