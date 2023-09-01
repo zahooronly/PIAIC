c380b9312b16f286b48c4330f24598960c2be59b
Zahoor Ahmad <i.am.zahoor.a@outlook.com>
2023-09-01 15:18:37 +0500

    assignment_2 program 2

---
+++ assignment_2/program2.js
@@ -0,0 +1,5 @@
+"use strict";
+const totalMarks = 500;
+const obtainedMarks = 375;
+const percentage = (obtainedMarks / totalMarks) * 100;
+console.log(`Percentage: ${percentage}%`);
---
+++ assignment_2/program2.ts
@@ -0,0 +1,5 @@
+const totalMarks: number = 500; 
+const obtainedMarks: number = 375; 
+
+const percentage: number = (obtainedMarks / totalMarks) * 100;
+console.log(`Percentage: ${percentage}%`);
