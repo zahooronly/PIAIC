c503e379b82166f4a09e4637db1ab5d033ccb96e
Zahoor Ahmad <i.am.zahoor.a@outlook.com>
2023-09-01 15:18:59 +0500

    assignment_2 program 6

---
+++ assignment_2/program6.js
@@ -0,0 +1,11 @@
+"use strict";
+const userAge = 16;
+if (userAge >= 0 && userAge <= 12) {
+    console.log("Child");
+}
+else if (userAge >= 13 && userAge <= 19) {
+    console.log("Teenager");
+}
+else {
+    console.log("Adult");
+}
---
+++ assignment_2/program6.ts
@@ -0,0 +1,9 @@
+const userAge: number = 16;
+
+if (userAge >= 0 && userAge <= 12) {
+  console.log("Child");
+} else if (userAge >= 13 && userAge <= 19) {
+  console.log("Teenager");
+} else {
+  console.log("Adult");
+}
