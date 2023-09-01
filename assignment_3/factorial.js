e455ae87a444f4bc838f0b8c05a45a130d7fec66
Zahoor Ahmad <i.am.zahoor.a@outlook.com>
2023-09-01 15:20:29 +0500

    assignment_3 factorial

---
+++ assignment_3/factorial.js
@@ -0,0 +1,12 @@
+"use strict";
+function calculateFactorial(n) {
+    let result = 1;
+    let i = 1;
+    while (i <= n) {
+        result *= i;
+        i++;
+    }
+    return result;
+}
+const factorialOf5 = calculateFactorial(5);
+console.log(`Factorial of 5 is ${factorialOf5}`);
