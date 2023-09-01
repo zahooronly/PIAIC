a2ff9413a41bbedaaf39729bf9b51165777ddc16
Zahoor Ahmad <i.am.zahoor.a@outlook.com>
2023-09-01 15:23:25 +0500

    assignment_5 fruits

---
+++ assignment_5/fruits.js
@@ -0,0 +1,4 @@
+"use strict";
+const fruits = ["apple", "banana", "cherry", "date", "grape"];
+const longFruits = fruits.filter(fruit => fruit.length > 5);
+console.log(longFruits);
---
+++ assignment_5/fruits.ts
@@ -0,0 +1,3 @@
+const fruits = ["apple", "banana", "cherry", "date", "grape"];
+const longFruits = fruits.filter(fruit => fruit.length > 5);
+console.log(longFruits);
