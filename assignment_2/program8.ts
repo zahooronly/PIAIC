b754602137ee0191911866c2567b39530b400df8
Zahoor Ahmad <i.am.zahoor.a@outlook.com>
2023-09-01 15:19:13 +0500

    assignment_2 program 8

---
+++ assignment_2/program8.ts
@@ -0,0 +1,11 @@
+const numberToCheck: number = 15;
+
+if (numberToCheck % 3 === 0 && numberToCheck % 5 === 0) {
+  console.log("Divisible by both 3 and 5");
+} else if (numberToCheck % 3 === 0) {
+  console.log("Divisible by 3");
+} else if (numberToCheck % 5 === 0) {
+  console.log("Divisible by 5");
+} else {
+  console.log("Not divisible by 3 or 5");
+}
