679b6fbc2634ad2dd9eda0f085e803414b81e430
Zahoor Ahmad <i.am.zahoor.a@outlook.com>
2023-09-01 15:19:21 +0500

    assignment_2 program 9

---
+++ assignment_2/program9.ts
@@ -0,0 +1,7 @@
+const year: number = 2024;
+
+if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+  console.log(`${year} is a leap year`);
+} else {
+  console.log(`${year} is not a leap year`);
+}
