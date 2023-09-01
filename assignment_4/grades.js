b8d99de6c9e1fc3fc88ad74db1ed32daf6b333b1
Zahoor Ahmad <i.am.zahoor.a@outlook.com>
2023-09-01 15:22:16 +0500

    assignment_4 grades

---
+++ assignment_4/grades.js
@@ -0,0 +1,9 @@
+"use strict";
+const grades = [65, 78, 32, 90, 45, 75, 22, 88, 95, 40];
+for (let i = 0; i < grades.length; i++) {
+    if (grades[i] < 50) {
+        grades.splice(i, 1);
+        i--;
+    }
+}
+console.log(grades);
