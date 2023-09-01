5e7a699302cc27b6186223066b50dc79e8046a3b
Zahoor Ahmad <i.am.zahoor.a@outlook.com>
2023-09-01 15:18:20 +0500

    assignment_1 program 1

---
+++ assignment_2/program1.js
@@ -0,0 +1,7 @@
+"use strict";
+let celsiusTemperature = 25;
+let fahrenheitTemperature = (celsiusTemperature * 9 / 5) + 32;
+console.log(`${celsiusTemperature}°C is equal to ${fahrenheitTemperature}°F`);
+let fahrenheitTemperature2 = 77;
+let celsiusTemperature2 = (fahrenheitTemperature2 - 32) * 5 / 9;
+console.log(`${fahrenheitTemperature2}°F is equal to ${celsiusTemperature2}°C`);
---
+++ assignment_2/program1.ts
@@ -0,0 +1,7 @@
+let celsiusTemperature: number = 25; 
+let fahrenheitTemperature: number = (celsiusTemperature * 9/5) + 32;
+console.log(`${celsiusTemperature}°C is equal to ${fahrenheitTemperature}°F`);
+
+let fahrenheitTemperature2: number = 77; 
+let celsiusTemperature2: number = (fahrenheitTemperature2 - 32) * 5/9;
+console.log(`${fahrenheitTemperature2}°F is equal to ${celsiusTemperature2}°C`);
