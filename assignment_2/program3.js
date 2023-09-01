688483e0bcbb5b825b7709d2416cb6d47e5880a2
Zahoor Ahmad <i.am.zahoor.a@outlook.com>
2023-09-01 15:18:45 +0500

    assignment_2 program 3

---
+++ assignment_2/program3.js
@@ -0,0 +1,5 @@
+"use strict";
+const days = 17;
+const weeks = Math.floor(days / 7);
+const remainingDays = days % 7;
+console.log(`${days} days = ${weeks} weeks and ${remainingDays} days`);
---
+++ assignment_2/program3.ts
@@ -0,0 +1,5 @@
+const days: number = 17;
+
+const weeks: number = Math.floor(days / 7);
+const remainingDays: number = days % 7;
+console.log(`${days} days = ${weeks} weeks and ${remainingDays} days`);
