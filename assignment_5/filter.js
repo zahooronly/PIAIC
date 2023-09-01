a28489698bb8ecc415cb53c285ab0b005ff17327
Zahoor Ahmad <i.am.zahoor.a@outlook.com>
2023-09-01 15:23:02 +0500

    assignment_5 filter

---
+++ assignment_5/filter.js
@@ -0,0 +1,4 @@
+"use strict";
+const listNumbers = [1, -2, 3, -4, 5];
+const positiveNumbers = listNumbers.filter(num => num >= 0);
+console.log(positiveNumbers);
---
+++ assignment_5/filter.ts
@@ -0,0 +1,3 @@
+const listNumbers:number[] = [1, -2, 3, -4, 5];
+const positiveNumbers = listNumbers.filter(num => num >= 0);
+console.log(positiveNumbers); 
