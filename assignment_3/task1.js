adeec14a26ed3b99d319e72f399aed91242cb1f1
Zahoor Ahmad <i.am.zahoor.a@outlook.com>
2023-09-01 15:21:27 +0500

    task1 deleted

--- assignment_3/task1.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-function spliceFunc(arr, index, value) {
-    return arr.splice(index, 0, value);
-}
-let myArray = [1, 2, 3, 4];
-console.log("Array: ", myArray);
-spliceFunc(myArray, 2, 4);
-console.log("New Array: ", myArray);
