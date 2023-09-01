88fbc22d6b0f2ff8b2f27a897f66776e6084ebd3
Zahoor Ahmad <i.am.zahoor.a@outlook.com>
2023-09-01 15:23:34 +0500

    assignment_5 map

---
+++ assignment_5/map.js
@@ -0,0 +1,4 @@
+"use strict";
+let numberss = [1, 2, 3, 4, 5];
+const doubledNumbers = numberss.map(num => num * 2);
+console.log(doubledNumbers);
---
+++ assignment_5/map.ts
@@ -0,0 +1,3 @@
+let numberss = [1, 2, 3, 4, 5];
+const doubledNumbers = numberss.map(num => num * 2);
+console.log(doubledNumbers);
