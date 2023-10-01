3c9cc73476d62700383c47354fa3658d60a20c44
ZahoorOnly <i.am.zahoor.a@outlook.com>
2023-10-01 15:46:08 +0500

    calculator logo and configuration

---
+++ calculator/logo.js
@@ -0,0 +1,22 @@
+const logo = `
+
+
+/$$$$$$            /$$
+/$$__  $$          | $$
+| $$  \__/  /$$$$$$ | $$
+| $$       |____  $$| $$
+| $$        /$$$$$$$| $$
+| $$    $$ /$$__  $$| $$
+|  $$$$$$/|  $$$$$$$| $$
+\______/  \_______/|__/
+
+
+Hy there, I'm a calculator. I can do the following operations:
+- Addition
+- Subtraction
+- Multiplication
+- Modulus
+- Division
+- Exponent
+`;
+export default logo;
---
+++ calculator/logo.ts
@@ -0,0 +1,23 @@
+const logo = `
+
+
+/$$$$$$            /$$
+/$$__  $$          | $$
+| $$  \__/  /$$$$$$ | $$
+| $$       |____  $$| $$
+| $$        /$$$$$$$| $$
+| $$    $$ /$$__  $$| $$
+|  $$$$$$/|  $$$$$$$| $$
+\______/  \_______/|__/
+
+
+Hy there, I'm a calculator. I can do the following operations:
+- Addition
+- Subtraction
+- Multiplication
+- Modulus
+- Division
+- Exponent
+`
+
+export default logo;
